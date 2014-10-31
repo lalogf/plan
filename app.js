9d30984695f16bba88da75e5de3cad14e5c6585c
lalogf <lalogf@air-de-manuel.attlocal.net>
2014-10-31 00:58:23 -0700

    Another one

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ passport.deserializeUser(function(obj, done) {
 
 
 passport.use(new GoogleStrategy({
-    returnURL: 'http://localhost:3000/auth/google/return',
-    realm: 'http://localhost:3000/'
+    returnURL: 'https://ghastly-ghost-7309.herokuapp.com/auth/google/return',
+    realm: 'https://ghastly-ghost-7309.herokuapp.com/'
   },
   function(identifier, profile, done) {
     // asynchronous verification, for effect...
